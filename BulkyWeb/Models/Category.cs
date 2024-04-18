98f0c40c717a9b00b1bcbf59a2e4e9f84a6ebefe
hp <hp@DESKTOP-5VO9MTH>
2024-04-18 09:40:30 +0200

    #all Good

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        //[Key]  - is not required as Id/ CategoryId here is kkey by default 
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
